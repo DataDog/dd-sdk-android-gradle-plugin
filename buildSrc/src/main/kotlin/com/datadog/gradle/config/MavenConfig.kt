c09b320ffcd355b168b8870a56897712ad68d481
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2022-08-30 10:10:55 +0200

    Prepare a new release

--- buildSrc/src/main/kotlin/com/datadog/gradle/config/MavenConfig.kt
+++ buildSrc/src/main/kotlin/com/datadog/gradle/config/MavenConfig.kt
@@ -18,7 +18,7 @@ import org.gradle.plugins.signing.SigningExtension
 
 object MavenConfig {
 
-    val VERSION = Version(1, 5, 0, Version.Type.Release)
+    val VERSION = Version(1, 5, 1, Version.Type.Release)
     const val GROUP_ID = "com.datadoghq"
     const val PUBLICATION = "pluginMaven"
 }
