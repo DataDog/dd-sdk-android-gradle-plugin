a6d3e5660219ad6374351d2f7fd3d048fbf3d868
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2024-01-08 16:56:40 +0100

    RUM-1822: Use snake case for properties in mapping file event

--- dd-sdk-android-gradle-plugin/src/main/kotlin/com/datadog/gradle/plugin/internal/OkHttpUploader.kt
+++ dd-sdk-android-gradle-plugin/src/main/kotlin/com/datadog/gradle/plugin/internal/OkHttpUploader.kt
@@ -103,8 +103,8 @@ internal class OkHttpUploader : Uploader {
         eventJson.put("version", identifier.version)
         eventJson.put("service", identifier.serviceName)
         eventJson.put("variant", identifier.variant)
-        eventJson.put("buildId", identifier.buildId)
-        eventJson.put("versionCode", identifier.versionCode)
+        eventJson.put("build_id", identifier.buildId)
+        eventJson.put("version_code", identifier.versionCode)
         eventJson.put("type", TYPE_JVM_MAPPING_FILE)
 
         val builder = MultipartBody.Builder()
